50c4c59f945b2bf74ba1ba1534975cff8f5727a7
Café Express Architect <architect@cafeexpress.com>
2026-06-07 13:10:57 +1000

    Update Google Sheets CSV URL source

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  menuCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT5K76-8O_1wD0FvXjV5-601J8aT7dY_S-bK1Y9f-X_84S7K5mXvL/pub?output=csv'
+  menuCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQg3zHchrSpOf-n-VlSJQkwOLgrnbt1Y4Im2zMp8cnRtWs496Ao3owEc0ymZ7tL8W7QBw1UmRnoJA1k/pub?output=csv'
 };
